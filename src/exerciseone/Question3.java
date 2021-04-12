5473eb2a5c70b23546976bbf27496a8b1693db50
Ash_Abera17 <Ash_Abera17@ashenafis-mbp.lan>
2021-04-12 13:39:27 -0400

    Initial Commit

---
+++ src/exerciseone/Question3.java
@@ -0,0 +1,18 @@
+package exerciseone;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Divide two integer numbers 
+		int num1 = 50;
+		int num2 = 3;
+		
+		int sub = num1 / num2;
+		
+		System.out.println("The division of num1 and num2 is = " + sub);
+
+	}
+
+}
